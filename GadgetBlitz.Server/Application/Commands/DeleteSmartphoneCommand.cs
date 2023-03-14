140d2700570dd65d8b51af94513bbcc8532124af
JusticeBeaver <fixu2@DESKTOP-MUFUCIH>
2023-03-14 17:55:26 +0100

    queries and commands

---
+++ GadgetBlitz.Server/Application/Commands/DeleteSmartphoneCommand.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using GadgetBlitz.Server.Core.Entities;
+using GadgetBlitz.Server.Core.Repositories;
+using MediatR;
+
+namespace GadgetBlitz.Server.Application.Commands
+{
+    public record DeleteSmartphoneCommand(Guid smartphoneId) : IRequest
+    {
+    }
+
+    public class DeleteSmartphoneCommandHandler : IRequestHandler<DeleteSmartphoneCommand>
+    {
+        private readonly ISmartphoneRepository _smartphoneRepository;
+
+        private readonly IMapper _mapper;
+        public DeleteSmartphoneCommandHandler(ISmartphoneRepository smartphoneRepository, IMapper mapper)
+        {
+            _smartphoneRepository = smartphoneRepository;
+            _mapper = mapper;
+        }
+        public async Task Handle(DeleteSmartphoneCommand request, CancellationToken cancellationToken)
+        {
+          
+
+            await _smartphoneRepository.DeleteAsync(request.smartphoneId);
+            
+        }
+
+        
+    }
+}
---
+++ GadgetBlitz.Server/Core/Queries/GetAllSmartphonesQuery.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.Runtime.Internal;
+using AutoMapper;
+using GadgetBlitz.Server.Core.Entities;
+using GadgetBlitz.Server.Core.Repositories;
+using MediatR;
+
+namespace GadgetBlitz.Server.Core.Queries
+{
+    public record GetAllSmartphonesQuery() : IRequest<List<Smartphone>>
+    {
+
+    }
+
+    public class GetAllSmartphonesQueryHandler : IRequestHandler<GetAllSmartphonesQuery, List<Smartphone>>
+    {
+        private readonly ISmartphoneRepository _smartphoneRepository;
+
+        private readonly IMapper _mapper;
+
+        public GetAllSmartphonesQueryHandler(ISmartphoneRepository smartphoneRepository, IMapper mapper)
+        {
+            _smartphoneRepository = smartphoneRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Smartphone>> Handle(GetAllSmartphonesQuery request, CancellationToken cancellationToken)
+        {
+            var smartphones = await _smartphoneRepository.GetAllAsync();
+            List<Smartphone> result = new List<Smartphone>(_mapper.Map<List<Smartphone>>(smartphones));
+            return result;
+        }
+    }
+    
+}
